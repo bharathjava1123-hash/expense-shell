5207759a6d17166ac12142622eacbb4c77a08e4e
bharathjava1123-hash <bharathjava1123@gmai.com>
2026-03-30 18:32:35 +0530

    changes

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 
-LOGS_FOLDER=/var/log/shell-script
+LOGS_FOLDER=/var/log/expense
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
